bc3de86bb549dbc8ff174cfc99be6a4760c925c7
jeckett <jeckett@cs.washington.edu>
2023-03-09 20:16:58 -0800

    initial commit; first draft of echo

---
+++ Nodes/EchoNode.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace Nodes;
+
+public class EchoNode
+{
+    private string? _nodeId;
+
+    private void Send(dynamic msg)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(msg));
+    }
+
+    private void Log(string s)
+    {
+        Console.Error.WriteLine(s);
+    }
+
+    public void ReceiveMessage(dynamic msg)
+    {
+        Log($"received msg [{msg}]");
+        if (msg.body.type == "init")
+        {
+            _nodeId = msg.body.node_id;
+            Log("sending init reply");
+            Send(new { src = _nodeId, dest = msg.src, body = new { type = "init_ok", in_reply_to = msg.body.msg_id } });
+        }
+        else if (msg.body.type == "echo")
+        {
+            Log("sending echo");
+            Send(new
+            {
+                src = _nodeId, dest = msg.src,
+                body = new { type = "echo_ok", in_reply_to = msg.body.msg_id, msg.body.echo }
+            });
+        }
+    }
+}
---
+++ Nodes/NodeRunner.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+using Nodes;
+
+var node = new EchoNode();
+while (true)
+{
+    var line = Console.In.ReadLine();
+    if (line != null) node.ReceiveMessage(JsonConvert.DeserializeObject<dynamic>(line));
+    Thread.Sleep(10);
+}
