717ead46f36d5b74fcce02fd1c48f5f6ea6a4d5e
jeckett <jeckett@cs.washington.edu>
2023-03-10 09:38:24 -0800

    Minor refactoring

--- Nodes/Echo/EchoNode.cs
+++ Nodes/Echo/EchoNode.cs
@@ -7,26 +7,17 @@ public class EchoNode
 {
     private string? _nodeId;
 
-    private void Send(dynamic msg)
-    {
-        Console.WriteLine(JsonConvert.SerializeObject(msg));
-    }
+    private void Send(dynamic msg) => Console.WriteLine(JsonConvert.SerializeObject(msg));
 
-    private void Log(string s)
-    {
-        Console.Error.WriteLine(s);
-    }
+    private void Log(string s) => Console.Error.WriteLine(s);
 
     public void ReceiveMessage(Message<InitPayload> msg)
     {
         _nodeId = msg.Body.NodeId;
-        var payload = new InitOkPayload(msg.Body.MsgId);
-        Send(new Message<InitOkPayload>(_nodeId, msg.Src, payload));
+        Send(new Message<InitOkPayload>(_nodeId, msg.Src, new InitOkPayload(msg.Body.MsgId)));
     }
 
-    public void ReceiveMessage(Message<EchoPayload> msg)
-    {
-        var payload = new EchoOkPayload(msg.Body.MsgId, msg.Body.MsgId, msg.Body.Echo);
-        Send(new Message<EchoOkPayload>(_nodeId, msg.Src, payload));
-    }
+    public void ReceiveMessage(Message<EchoPayload> msg) =>
+        Send(new Message<EchoOkPayload>(_nodeId, msg.Src,
+            new EchoOkPayload(msg.Body.MsgId, msg.Body.MsgId, msg.Body.Echo)));
 }
