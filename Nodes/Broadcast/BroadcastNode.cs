81a443d197a553fe538e61e96aec6d3563115bd3
jeckett <jeckett@cs.washington.edu>
2023-09-16 13:18:41 -0700

    Add basic BroadcastNode

--- Nodes/Broadcast/BroadcastNode.cs
+++ Nodes/Broadcast/BroadcastNode.cs
@@ -5,35 +5,32 @@ namespace Nodes.Broadcast;
 public class BroadcastNode : Node
 {
     private ImmutableHashSet<long> _messages = ImmutableHashSet<long>.Empty;
-    private HashSet<string> _neighbors = new();
     
     private static int Next(ref int messageId) => ++messageId;
     private int _messageId = -1;
-
-    [MessageHandler("update")]
-    public void HandleUpdate(dynamic msg)
-    {
-        HashSet<long> update = msg.Body.Update.ToObject<HashSet<long>>();
-        ImmutableInterlocked.Update(ref _messages, messages => messages.Union(update));
-    }
+    private static bool IsFromClient(dynamic msg) => msg.Src.ToString().ToLower().StartsWith("c");
 
     [MessageHandler("broadcast")]
     public void HandleBroadcast(dynamic msg)
     {
-        var message = (int)msg.Body.Message;
+        var message = (long)msg.Body.Message;
         ImmutableInterlocked.Update(ref _messages, messages => messages.Add(message));
-        foreach (var nodeId in NodeIds)
+        if (IsFromClient(msg))
         {
-            if (nodeId != NodeId)
+            foreach (var nodeId in NodeIds)
             {
-                WriteMessage(new
+                if (nodeId != NodeId)
                 {
-                    Src = NodeId,
-                    Dest = nodeId,
-                    Body = new { Type = "update", Update = _messages, MsgId = Next(ref _messageId) }
-                });
+                    SendRequest(new
+                    {
+                        Src = NodeId,
+                        Dest = nodeId,
+                        Body = new { Type = "broadcast", Message = message, MsgId = Next(ref _messageId) }
+                    });
+                }
             }
         }
+
         WriteResponse(msg, new { Type = "broadcast_ok", InReplyTo = msg.Body.MsgId });
     }
 
--- Nodes/Broadcast/DishonestBroadcastNode.cs
+++ Nodes/Broadcast/DishonestBroadcastNode.cs
@@ -2,35 +2,38 @@ using System.Collections.Immutable;
 
 namespace Nodes.Broadcast;
 
-public class SimpleBroadcastNode : Node
+// Sends all messages to all other nodes after receiving a broadcast. Not the intended way to solve the challenge
+public class DishonestBroadcastNode : Node
 {
     private ImmutableHashSet<long> _messages = ImmutableHashSet<long>.Empty;
     
     private static int Next(ref int messageId) => ++messageId;
     private int _messageId = -1;
-    private static bool IsFromClient(dynamic msg) => msg.Src.ToString().ToLower().StartsWith("c");
+
+    [MessageHandler("update")]
+    public void HandleUpdate(dynamic msg)
+    {
+        HashSet<long> update = msg.Body.Update.ToObject<HashSet<long>>();
+        ImmutableInterlocked.Update(ref _messages, messages => messages.Union(update));
+    }
 
     [MessageHandler("broadcast")]
     public void HandleBroadcast(dynamic msg)
     {
-        var message = (long)msg.Body.Message;
+        var message = (int)msg.Body.Message;
         ImmutableInterlocked.Update(ref _messages, messages => messages.Add(message));
-        if (IsFromClient(msg))
+        foreach (var nodeId in NodeIds)
         {
-            foreach (var nodeId in NodeIds)
+            if (nodeId != NodeId)
             {
-                if (nodeId != NodeId)
+                WriteMessage(new
                 {
-                    SendRequest(new
-                    {
-                        Src = NodeId,
-                        Dest = nodeId,
-                        Body = new { Type = "broadcast", Message = message, MsgId = Next(ref _messageId) }
-                    });
-                }
+                    Src = NodeId,
+                    Dest = nodeId,
+                    Body = new { Type = "update", Update = _messages, MsgId = Next(ref _messageId) }
+                });
             }
         }
-
         WriteResponse(msg, new { Type = "broadcast_ok", InReplyTo = msg.Body.MsgId });
     }
 
